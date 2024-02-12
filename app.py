601d679b281f4235b298476e1a993beaebf3d2f6
Umer Arshad <umer@breu.io>
2024-02-12 23:27:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_geek():
             <h1>Hello from Quantm test 1 repo</h1>
             <h2>10-Feb-2024</h2>
             <h2>Hello</h2>
-            <h3>PR testing with mahad</h3>
+            <h3>Test for #88</h3>
             """
 
 
